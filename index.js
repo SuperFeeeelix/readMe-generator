9b6e4c3da31ce9ee5796ab90e03a804fcc53fb19
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-05-31 21:38:29 -0700

    removed some files to create my render license badge etc

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    question: "",
+    answers: []
+    
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
