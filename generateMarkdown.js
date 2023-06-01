6e30cc6b25a066800c67b7c766066bab906f250f
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-06-01 02:17:29 -0700

    added my functions for my MIT Licenses

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -10,11 +10,26 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+}
+  else{
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "MIT") {
+    return `## License`;
+  }
+  else {
+    return "";
+  }
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
