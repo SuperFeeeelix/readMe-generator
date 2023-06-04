314f5fc0bc949c38c14e40b1b1383ccc0f30e371
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-06-04 04:07:32 -0700

    deleted files that i copies from the UCSD file (node_moduels) to then delete the packages becuase my generate markdown was not finished. completed tmy generate make down to create my readme and added the file to my indexjs

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,8 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === "MIT") {
-    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  if (license !== "None") {
+    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
   }
   return "";
 }
@@ -11,10 +11,10 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === "MIT") {
+  if (license !== "none") {
     return "https://opensource.org/licenses/MIT";
-}
-  else{
+  }
+  else {
     return "";
   }
 }
@@ -22,10 +22,9 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === "MIT") {
-    return `## License`;
-  }
-  else {
+  if (license !== "none") {
+    return "## License"
+  } else {
     return "";
   }
 
@@ -34,7 +33,33 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  by ${data.name}
+  ${renderLicenseBadge(data.license)}
+  ## Table Of Contents
+  * [Description](#description)
+  * [Requirements](#requirements)
+  * [Licenses](#Licenses)
+  * [Contributors](#contributors)
+  * [Contact-Me](#contact-me)
+  * [Testing](#testing)
+  ${renderLicenseLink(data.license)}
+  ## Description
+  ${data.description}
+  ## Requirements
+  ${data.require}
+  ## Usage
+  ${data.usage}
+  ## Contact-Me
+  * Name - ${data.name}
+  * Email - ${data.email}
+  * Github - [${data.creator}](https://github.com/${data.creator}/)
+  ## Contributors
+  ${data.contributors}
+  ## Testing
+  \\\
+  ${data.test}
+  \\\
+  ${renderLicenseSection(data.license)}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // TODO: Include packages needed for this application
-
+const generateMarkdown = require("./generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [{
-    question: "",
+    question: "What is the titile of the project?",
     answers: []
-    
-}];
+
+}]; 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeToFile(fileName,data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log("you did it")
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
